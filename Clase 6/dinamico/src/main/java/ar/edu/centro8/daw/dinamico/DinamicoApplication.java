c77a0f3395ce28866c01149676442c066311e0da
cfpit <127329179+cfpit@users.noreply.github.com>
2025-09-18 16:03:53 -0300

    Add files via upload

---
+++ Clase 6/dinamico/src/main/java/ar/edu/centro8/daw/dinamico/DinamicoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.centro8.daw.dinamico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinamicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinamicoApplication.class, args);
+	}
+
+}
