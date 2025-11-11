6911d1399918d3976b1b7aedc716b765b9dc64c7
cfpit <127329179+cfpit@users.noreply.github.com>
2025-11-11 19:41:24 -0300

    Delete Clase 13/proyectojpa1an directory

--- Clase 13/proyectojpa1an/src/main/java/ar/edu/centro8/desarrollo/proyectojpa1an/repositories/AutoRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.centro8.desarrollo.proyectojpa1an.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ar.edu.centro8.desarrollo.proyectojpa1an.models.Auto;
-
-public interface AutoRepository extends JpaRepository<Auto, Long>{
-
-}
--- Clase 13/proyectojpa1an/src/main/java/ar/edu/centro8/desarrollo/proyectojpa1an/repositories/ConcesionariaRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.centro8.desarrollo.proyectojpa1an.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ar.edu.centro8.desarrollo.proyectojpa1an.models.Concesionaria;
-
-public interface ConcesionariaRepository extends JpaRepository<Concesionaria,Long>{
-
-}
